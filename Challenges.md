04b0d322381833d4000affd9e7735947c8f149b8
Igor Figueiredo Silva <igorfigueiredors@gmail.com>
2021-04-15 14:13:42 -0300

    initial commit

---
+++ Challenges.md
@@ -0,0 +1,4 @@
+<h1 align="center">Desafios da maratona</h1>
+<h2 align="center">Aula 1</h2>
+
+- Reaproveitar linhas de código com EJS template engine, como por exemplo a tag &lt;head&gt; dos arquivos ejs.
---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = require("./routes");
+const app = express();
+
+// setando a template engine ejs
+app.set('view engine', 'ejs');
+
+// habilitar arquivos statics
+app.use(express.static('public'));
+
+// usando rotas do routes.js
+app.use(routes)
+
+// ouve a rota e liga o servidor na rota pedida
+app.listen(8080, () => console.log('8080 is a magic port ❤️'));
+
